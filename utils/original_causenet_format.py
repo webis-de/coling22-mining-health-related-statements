acc66885e4b252214a2490980620e0bd2885d525
Ferdinand Schlatt <ferdinand.schlatt@informatik.uni-halle.de>
2022-09-27 16:39:47 +0200

    fix saving

--- utils/original_causenet_format.py
+++ utils/original_causenet_format.py
@@ -54,30 +54,36 @@ def main(args=None):
             effect = effect.strip()
             health_causenet[(cause, effect)] = [value == "True" for value in split[2:]]
 
+    num_statements = {name: 0 for name in names}
+
     with bz2.open(args.causenet_jsonl_file, mode="rt") as json_line_file:
         writer = Writer(args.health_causenet_out_dir, names)
-        for jsonl_line in tqdm(json_line_file, total=11609890):
-            relation_dict = json.loads(jsonl_line)
-            relation = relation_dict["causal_relation"]
-            cause = relation["cause"]["concept"].replace("_", " ").strip()
-            cause = re.sub(r" +", r" ", cause)
-            effect = relation["effect"]["concept"].replace("_", " ").strip()
-            effect = re.sub(r" +", r" ", effect)
-            if "support" not in relation_dict:
-                pattern_set = set()
-                for source in relation_dict["sources"]:
-                    source_type = source["type"]
-                    if source_type in ("wikipedia_list", "wikipedia_infobox"):
-                        continue
-                    if pattern_set is not None:
-                        pattern_set.add(source["payload"]["path_pattern"])
-                relation_dict["support"] = len(pattern_set)
-            if (cause, effect) in health_causenet:
-                for name, health_related in zip(
-                    names, health_causenet[(cause, effect)]
-                ):
-                    if health_related:
-                        writer.write(name, json.dumps(relation_dict) + "\n")
+        with writer:
+            for jsonl_line in tqdm(json_line_file, total=11609890):
+                relation_dict = json.loads(jsonl_line)
+                relation = relation_dict["causal_relation"]
+                cause = relation["cause"]["concept"].replace("_", " ").strip()
+                cause = re.sub(r" +", r" ", cause)
+                effect = relation["effect"]["concept"].replace("_", " ").strip()
+                effect = re.sub(r" +", r" ", effect)
+                if "support" not in relation_dict:
+                    pattern_set = set()
+                    for source in relation_dict["sources"]:
+                        source_type = source["type"]
+                        if source_type in ("wikipedia_list", "wikipedia_infobox"):
+                            continue
+                        if pattern_set is not None:
+                            pattern_set.add(source["payload"]["path_pattern"])
+                    relation_dict["support"] = len(pattern_set)
+                if (cause, effect) in health_causenet:
+                    for name, health_related in zip(
+                        names, health_causenet[(cause, effect)]
+                    ):
+                        if health_related:
+                            num_statements[name] += 1
+                            writer.write(name, json.dumps(relation_dict) + "\n")
+
+    print(json.dumps(num_statements, indent=2))
 
 
 if __name__ == "__main__":
